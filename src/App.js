97a2a7b7f144af61786151b79a53eef008cf6645
Vicky Rodriguez <vrod8763@gmail.com>
2020-09-01 16:33:17 -0400

    create SearchParam

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import Pet from "./Pet"
+import SearchParams from './SearchParams'
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Little P" animal="Dog" breed="Poodle Mix" />
+      <SearchParams />
+      {/* <Pet name="Little P" animal="Dog" breed="Poodle Mix" />
       <Pet name="Bailey" animal="Dog" breed="Terrier Mix" />
-      <Pet name="Joise" animal="Dog" breed="Pitbull Mix" />
+      <Pet name="Joise" animal="Dog" breed="Pitbull Mix" /> */}
     </div>
   )
   }
