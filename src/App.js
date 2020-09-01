da976f2b8bccca088dc0ccb9f819e5b3b37225e0
Vicky Rodriguez <vrod8763@gmail.com>
2020-09-01 13:27:39 -0400

    update linting and formatting

--- src/App.js
+++ src/App.js
@@ -2,35 +2,28 @@ const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ])
-}
+    React.createElement("h2", {}, breed),
+  ]);
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Little P",
-        animal: "dog",
-        breed: "Mixed"
-      }),
-      React.createElement(Pet, {
-        name: "Bailey",
-        animal: "dog",
-        breed: "Mixed"
-      }),
-      React.createElement(Pet, {
-        name: "Josie",
-        animal: "dog",
-        breed: "Mixed"
-      })
-    ]
-  )
-}
-ReactDOM.render(
-  React.createElement(App),
-document.getElementById("root")
-)
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Little P",
+      animal: "Dog",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Bailey",
+      animal: "Dog",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Josie",
+      animal: "D og",
+      breed: "Mixed",
+    }),
+  ]);
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
