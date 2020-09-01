8d50ec8ae69c0c970d67c68918e21b464b72931c
Vicky Rodriguez <vrod8763@gmail.com>
2020-09-01 17:14:53 -0400

    update animal dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-
+  const [animal, setAnimal] = useState("Dog");
   return (
     <div className="search-params">
       <form>
@@ -12,8 +13,22 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            onChange={evt => setLocation(evt.target.value)}
+            onChange={(evt) => setLocation(evt.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(evt) => setAnimal(evt.target.value)}
+            onBlur={(evt) => setAnimal(evt.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
