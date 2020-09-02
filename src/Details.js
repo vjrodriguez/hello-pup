eee9bea2f8c9c8abc5ac55f57fa23d1f4782b0cb
Vicky Rodriguez <vrod8763@gmail.com>
2020-09-02 15:23:35 -0400

    Update modal and toggleModal in Details.js

--- src/Details.js
+++ src/Details.js
@@ -4,15 +4,19 @@ import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import { navigate } from "@reach/router";
+import Modal from './Modal'
+
 
 class Details extends React.Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
   componentDidMount() {
     // throw new Error('test error bounday!')
     pet
       .animal(+this.props.id)
       .then(({ animal }) => {
         this.setState({
+          url: animal.url,
           name: animal.name,
           animal: animal.type,
           location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -24,12 +28,15 @@ class Details extends React.Component {
       })
       .catch((err) => this.setState({ error: err }));
   }
+
+  toggleModal =()=> this.setState({showModal: !this.state.showModal})
+  adopt = ()=> navigate(this.state.url)
   render() {
     if (this.state.loading) {
       return <h1>loading … </h1>;
     }
 
-    const { animal, breed, location, description, media, name } = this.state;
+    const { animal, breed, location, description, media, name, showModal } = this.state;
 
     return (
       <div className="details">
@@ -39,10 +46,23 @@ class Details extends React.Component {
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
           <ThemeContext.Consumer>
             {(themeHook) => (
-            <button style={{backgroundColor: themeHook[0]}}>Adopt {name}</button>)}
+            <button onClick={this.toggleModal} style={{backgroundColor: themeHook[0]}}>Adopt {name}</button>)}
           </ThemeContext.Consumer>
 
           <p>{description}</p>
+          {
+            showModal? (
+              <Modal>
+                <div>
+                <h1>Would you like to adopt {name}?</h1>
+        <div className="buttons">
+          <button onClick={this.adopt}>Yes</button>
+          <button onClick={this.toggleModal}>Not Right Now</button>
+        </div>
+                </div>
+              </Modal>
+            ) : null
+          }
         </div>
       </div>
     );
